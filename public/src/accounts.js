79e2c478e0bd43a1d039d13e0f4356318dcef7e3
HasnaaElid <140899049+HasnaaElid@users.noreply.github.com>
2023-09-23 09:02:54 -0500

    Update accounts.js

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,16 +1,17 @@
 function findAccountById(accounts, id) {
-  // YOUR SOLUTION HERE
-  // Hint: You can use the [`find()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find) method here. 
+  let found = accounts.find((account) => account.id === id);
+  return found;
 }
 
 function sortAccountsByLastName(accounts) {
-  // YOUR SOLUTION HERE
-  // Hint: You can use the [`sort()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort) method here. 
+  accounts.sort((accountA, accountB) =>
+    accountA.name.last.toLowerCase() >  accountB.name.last.toLowerCase() ? 1 : -1);
+  return accounts;
 }
 
 function getAccountFullNames(accounts) {
-  // YOUR SOLUTION HERE
-  // Hint: You can use the [`map()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map) method here.
+  let accountName = accounts.map((account) => `${account.name.first} ${account.name.last}` );
+  return accountName;
 }
 
 // NOTE: YOU DON'T HAVE TO EDIT THE FUNCTIONS BELOW
